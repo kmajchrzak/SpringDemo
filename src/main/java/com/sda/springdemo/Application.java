d695be6bf33e5e29c8801f1836de98d555c1f45c
Karolina Majchrzak <karolina.majchrzak966@gmail.com>
2023-03-12 12:33:01 +0100

    implement FileRepository class

--- src/main/java/com/sda/springdemo/Application.java
+++ src/main/java/com/sda/springdemo/Application.java
@@ -1,11 +1,14 @@
 package com.sda.springdemo;
 
 public class Application {
+    private final FileRepository fileRepository = new FileRepository();
+
     public void start(){
         System.out.println("Start programu");
-
+        fileRepository.read();
     }
     public void end(){
         System.out.println("Koniec programu");
+        fileRepository.write();
     }
 }
---
+++ src/main/java/com/sda/springdemo/FileRepository.java
@@ -0,0 +1,11 @@
+package com.sda.springdemo;
+
+public class FileRepository {
+    public void read(){
+        System.out.println("Read from file");
+
+    }
+    public void write(){
+        System.out.println("Write into file");
+    }
+}
