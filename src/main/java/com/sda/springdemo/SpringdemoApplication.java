90b68e6993b8e799f25ca5502749d0028af32518
Karolina Majchrzak <karolina.majchrzak966@gmail.com>
2023-03-12 12:26:50 +0100

    implement Application class

--- src/main/java/com/sda/springdemo/SpringdemoApplication.java
+++ src/main/java/com/sda/springdemo/SpringdemoApplication.java
@@ -3,11 +3,15 @@ package com.sda.springdemo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+//@SpringBootApplication
 public class SpringdemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringdemoApplication.class, args);
+		//SpringApplication.run(SpringdemoApplication.class, args);
+
+		Application application = new Application();
+		application.start();
+		application.end();
 	}
 
 }
