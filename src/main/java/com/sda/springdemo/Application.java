eeee04401d8dfa725a22a492367d5927c5c36576
Karolina Majchrzak <karolina.majchrzak966@gmail.com>
2023-03-12 12:40:58 +0100

    replace FileRepository with DbRepository

--- src/main/java/com/sda/springdemo/Application.java
+++ src/main/java/com/sda/springdemo/Application.java
@@ -1,14 +1,15 @@
 package com.sda.springdemo;
 
 public class Application {
-    private final FileRepository fileRepository = new FileRepository();
+   // private final FileRepository fileRepository = new FileRepository();
+    private final DbRepository dbRepository = new DbRepository();
 
     public void start(){
         System.out.println("Start programu");
-        fileRepository.read();
+        dbRepository.read();
     }
     public void end(){
         System.out.println("Koniec programu");
-        fileRepository.write();
+        dbRepository.write();
     }
 }
---
+++ src/main/java/com/sda/springdemo/DbRepository.java
@@ -0,0 +1,11 @@
+package com.sda.springdemo;
+
+public class DbRepository {
+    public void read(){
+        System.out.println("Read from Db");
+
+    }
+    public void write(){
+        System.out.println("Write into Db");
+    }
+}
